ff182924ecb109483b8a33f6e6badebc6f3c5f8b
Lars <larlars@larspire.(none)>
2011-03-29 19:23:46 +0200

    removed debugging mode from setupmesh.sh and added a shutdown of network-manager

--- setup/setupmesh.sh
+++ setup/setupmesh.sh
@@ -10,9 +10,10 @@ if [ -z $WIFNAME ]; then
 	zenity --error --title "NO WIRELESS INTERFACE" --text "ERROR: You don't have any wireless network interface"
 	exit 1
 else
-	echo gksudo ifconfig $WIFNAME down
-	echo gksudo iwconfig mode Ad-Hoc essid $SSID key s:$KEY
-	echo gksudo avahi-autoipd --check $WIFNAME 
+	gksudo service network-manager stop
+	gksudo ifconfig $WIFNAME down
+	gksudo iwconfig mode Ad-Hoc essid $SSID key s:$KEY
+	gksudo avahi-autoipd --check $WIFNAME 
 	if [ "$?" == "0" ]; then
 		echo gksudo avahi-autoipd --refresh $WIFNAME
 	else
