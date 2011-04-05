60a362bf0e4de76185da97e08e2797c241dc464e
Anders Sundman <anders@4zm.org>
2011-04-05 08:47:15 +0200

    Added GPL premable

--- setup/setupmesh.sh
+++ setup/setupmesh.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-
+# Copyright (c) 2011 Lars Larsson <lars.la@gmail.com>
+# 
+# This file is part of dandelion
+# 
+# dandelion is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# dandelion is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with dandelion.  If not, see <http://www.gnu.org/licenses/>.
 
 SSID=$(zenity --entry --text "SSID of the network" --title "SSID")
 
