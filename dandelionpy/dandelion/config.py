af5e0c94c6a8ed16c1ab3dccc9781baf65b106b3
Anders Sundman <anders@4zm.org>
2011-07-28 21:32:40 +0200

    Added id section to config file.
    It is now possible to claim the identity used the last time the program was rum. If no identity is specified, a new one will be generated.

--- dandelionpy/dandelion/config.py
+++ dandelionpy/dandelion/config.py
@@ -20,7 +20,7 @@ along with Dandelion.  If not, see <http://www.gnu.org/licenses/>.
 from dandelion.database import ContentDB
 import configparser
 import dandelion.identity
-import tempfile
+from dandelion.util import decode_b64_bytes, encode_b64_bytes
 
 class ConfigException(Exception):
     pass
@@ -32,31 +32,31 @@ class Config:
         pass
 
 class ServerConfig(Config):
-    
+
     _SECTION_NAME = 'server'
-    
+
     _PORT_NAME = 'port'
     _PORT_DEFAULT = 1337
-    
+
     _IP_NAME = 'ip'
     _IP_DEFAULT = '0.0.0.0' # Bind to anything
 
     _DB_FILE_NAME = "db_file"
     _DB_FILE_DEFAULT = "dandelion.sqlite"
-    
+
     def __init__(self):
         self._port = ServerConfig._PORT_DEFAULT
         self._ip = ServerConfig._IP_DEFAULT
         self._db_file = ServerConfig._DB_FILE_DEFAULT
-        
+
     @property
     def port(self):
         return self._port
-    
+
     @port.setter
     def port(self, value):
         self._port = value
-          
+
     @property
     def ip(self):
         return self._ip
@@ -64,7 +64,7 @@ class ServerConfig(Config):
     @ip.setter
     def ip(self, value):
         self._ip = value
-        
+
     @property
     def db_file(self):
         return self._db_file
@@ -72,11 +72,11 @@ class ServerConfig(Config):
     def load(self, confparser):
         if not confparser.has_section(ServerConfig._SECTION_NAME):
             confparser.add_section(ServerConfig._SECTION_NAME)
-        
+
         # TODO for python 3.2; replace with fallback arg.
         if confparser.has_option(ServerConfig._SECTION_NAME, ServerConfig._PORT_NAME):
             self._port = confparser.getint(ServerConfig._SECTION_NAME, ServerConfig._PORT_NAME)
-        
+
         if confparser.has_option(ServerConfig._SECTION_NAME, ServerConfig._IP_NAME):
             self._ip = confparser.get(ServerConfig._SECTION_NAME, ServerConfig._IP_NAME)
 
@@ -91,16 +91,16 @@ class ServerConfig(Config):
 
 
 class SynchronizerConfig(Config):
-    
+
     _SECTION_NAME = 'synchronizer'
-        
+
     def __init__(self):
         pass
-        
+
     def load(self, confparser):
         if not confparser.has_section(SynchronizerConfig._SECTION_NAME):
             raise ConfigException
-        
+
 
     def store(self, confparser):
         confparser.add_section(SynchronizerConfig._SECTION_NAME)
@@ -121,18 +121,18 @@ class DiscovererConfig(Config):
         confparser.add_section(DiscovererConfig._SECTION_NAME)
 
 class UiConfig(Config):
-    
+
     _SECTION_NAME = 'ui'
 
     # TODO button sizes etc.
-    
+
     # master
 
     _BG_MASTER_NAME = 'bg_master'
     _BG_MASTER_DEFAULT = 'black'
 
     # welcome screen
-    
+
     _WELCOME_SCREEN_NAME = 'welcome_screen'
     _WELCOME_SCREEN_DEFAULT = """  
   Welcome to the Dandelion Message System  
@@ -147,7 +147,7 @@ class UiConfig(Config):
   any existing infrastructure like the Internet or   
   mobile phone services - it is truly peer to peer. 
         """
-    
+
     # button
 
     _BG_BUTTON_NAME = 'bg_button'
@@ -163,13 +163,13 @@ class UiConfig(Config):
     _HBG_BUTTON_DEFAULT = 'yellow'
 
     # windows widgets
-    
+
     _BG_WINDOW_NAME = 'bg_window'
     _BG_WINDOW_DEFAULT = 'black'
 
     _FG_WINDOW_NAME = 'fg_window'
     _FG_WINDOW_DEFAULT = 'green'
-        
+
     # button alert 
     #fg_button_a = "red" # foreground color
 
@@ -215,7 +215,7 @@ class UiConfig(Config):
 
     _LABLE_SET_NEW_NICK_NAME = 'lable_set_new_nick'
     _LABLE_SET_NEW_NICK_DEFAULT = 'Set new nick'
-    
+
     _LABLE_SEARCHBOX_NAME = 'lable_searchbox'
     _LABLE_SEARCHBOX_DEFAULT = 'Search'
 
@@ -227,7 +227,7 @@ class UiConfig(Config):
             UiConfig._FG_BUTTON_NAME : UiConfig._FG_BUTTON_DEFAULT,
             UiConfig._ABG_BUTTON_NAME : UiConfig._ABG_BUTTON_DEFAULT,
             UiConfig._HBG_BUTTON_NAME : UiConfig._HBG_BUTTON_DEFAULT,
-            UiConfig._BG_WINDOW_NAME : UiConfig._BG_WINDOW_DEFAULT, 
+            UiConfig._BG_WINDOW_NAME : UiConfig._BG_WINDOW_DEFAULT,
             UiConfig._FG_WINDOW_NAME : UiConfig._FG_WINDOW_DEFAULT,
             UiConfig._FG_BUTTON_A_NAME : UiConfig._FG_BUTTON_A_DEFAULT,
             UiConfig._LABLE_SEARCH_M_NAME : UiConfig._LABLE_SEARCH_M_DEFAULT,
@@ -244,7 +244,7 @@ class UiConfig(Config):
             UiConfig._LABLE_SET_NEW_NICK_NAME : UiConfig._LABLE_SET_NEW_NICK_DEFAULT,
             UiConfig._LABLE_SEARCHBOX_NAME : UiConfig._LABLE_SEARCHBOX_DEFAULT}
 
-    def __getitem__(self, key): 
+    def __getitem__(self, key):
         return self.uidict[key]
 
     def __setitem__(self, key, value):
@@ -254,7 +254,7 @@ class UiConfig(Config):
         if not confparser.has_section(UiConfig._SECTION_NAME):
             #raise ConfigException
             confparser.add_section(UiConfig._SECTION_NAME)
-        
+
         for key in confparser[UiConfig._SECTION_NAME]:
             if key in self.uidict:
                 self.uidict[key] = confparser[UiConfig._SECTION_NAME][key]
@@ -268,19 +268,33 @@ class UiConfig(Config):
             confparser.set(UiConfig._SECTION_NAME, key, self.uidict[key])
 
 class IdentityConfig(Config):
-    
+
     _SECTION_NAME = 'identity'
-        
+
+    _MY_ID_NAME = 'myid'
+    _MY_ID_DEFAULT = None
+
     def __init__(self):
-        pass
-        
+      self._my_id = IdentityConfig._MY_ID_DEFAULT
+
+    @property
+    def my_id(self):
+        return self._my_id
+
+    @my_id.setter
+    def my_id(self, value):
+        self._my_id = value
+
     def load(self, confparser):
         if not confparser.has_section(IdentityConfig._SECTION_NAME):
-            raise ConfigException
-        
+            confparser.add_section(IdentityConfig._SECTION_NAME)
+
+        if confparser.has_option(IdentityConfig._SECTION_NAME, IdentityConfig._MY_ID_NAME):
+          self._my_id = confparser.get(IdentityConfig._SECTION_NAME, IdentityConfig._MY_ID_NAME)
 
     def store(self, confparser):
         confparser.add_section(IdentityConfig._SECTION_NAME)
+        confparser.set(IdentityConfig._SECTION_NAME, IdentityConfig._MY_ID_NAME, self._my_id)
 
 class ConfigManager:
 
@@ -291,12 +305,26 @@ class ConfigManager:
         self._discoverer_config = DiscovererConfig()
         self._id_manager_config = IdentityConfig()
         self._ui_config = UiConfig()
-        
+
         self.read_file()
-        
+
         self._content_db = ContentDB(self._server_config.db_file)
-        self._identity = dandelion.identity.generate() # Should read from DB if available
-        self._content_db.add_identities([self._identity])
+
+        if self._id_manager_config.my_id is not None and not self._content_db.contains_identity(decode_b64_bytes(self._id_manager_config.my_id.encode())) :
+            print("WARNING! Bad or non existing ID requested in config. Requested:", self._id_manager_config.my_id)
+            self._id_manager_config.my_id = None
+
+        if self._id_manager_config.my_id is None:
+            self._identity = dandelion.identity.generate()
+            self._content_db.add_identities([self._identity])
+            id_str = encode_b64_bytes(self._identity.fingerprint).decode()
+            self._id_manager_config.my_id = id_str
+            print("My new ID:", id_str)
+
+        else:
+            self._identity = self._content_db.get_identities(fingerprints=[decode_b64_bytes(self._id_manager_config.my_id.encode())])[0]
+            print("My claimed ID:", self._id_manager_config.my_id)
+
 
     @property
     def config_file(self):
@@ -305,11 +333,11 @@ class ConfigManager:
     @property
     def server_config(self):
         return self._server_config
-    
+
     @property
     def synchronizer_config(self):
         return self._synchronizer_config
-    
+
     @property
     def discoverer_config(self):
         return self._discoverer_config
@@ -317,33 +345,36 @@ class ConfigManager:
     @property
     def identity_manager_config(self):
         return self._id_manager_config
-    
+
     @property
     def ui_config(self):
         return self._ui_config
-    
-    @property 
+
+    @property
     def content_db(self):
         return self._content_db
-    
-    @property 
+
+    @property
     def identity(self):
         return self._identity
 
     def write_file(self):
 
         confparser = configparser.ConfigParser()
-        
+
         self._server_config.store(confparser)
         self._ui_config.store(confparser)
+        self._id_manager_config.store(confparser)
 
         with open(self._cfg_file_name, 'w') as configfile:
             confparser.write(configfile)
-    
+
     def read_file(self):
-        
+
         confparser = configparser.ConfigParser()
         confparser.read(self._cfg_file_name)
-        
+
         self._server_config.load(confparser)
         self._ui_config.load(confparser)
+        self._id_manager_config.load(confparser)
+
